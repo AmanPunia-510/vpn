d9da5a3a0a73fa2cf2a27ab4fc48322c1ab573d3
AmanPunia-510 <puniaaman888@gmail.com>
2024-02-29 20:34:54 +0530

    change in lets or choose margin

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector(".menubar");
+let navbar = document.querySelector(".navbar-items");
+
+menu.addEventListener("click", function () {
+    navbar.classList.toggle("show");
+})
