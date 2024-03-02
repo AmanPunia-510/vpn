e2b6f24be5ea12f3c96dfa2117b13c763d0bf2d9
AmanPunia-510 <puniaaman888@gmail.com>
2024-03-02 10:14:02 +0530

    make slick slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,34 @@
-let menu = document.querySelector(".menubar");
-let navbar = document.querySelector(".navbar-items");
-
-menu.addEventListener("click", function () {
-    navbar.classList.toggle("show");
-})
+$('.slick-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
